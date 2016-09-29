8cbac20c24c3f677243e42b01af874c226984e8a
Sthefany <Sthefany>
2016-09-29 11:04:13 -0500

    solucionando errores de linter

--- js/app.js
+++ js/app.js
@@ -1,66 +1,65 @@
-window.onload = function(){
-	var contenedor = document.getElementById("contenedor");
-	var btnNuevaTarea = document.getElementById("btnNuevaTarea");
-	var tarea = document.getElementById("tarea");
+window.onload = function () {
+
+	var contenedor = document.getElementById("contenedor"),
+		btnNuevaTarea = document.getElementById("btnNuevaTarea"),
+		tarea = document.getElementById("tarea");
 	btnNuevaTarea.disabled = true;
 
-	tarea.addEventListener("keydown",function(){
-		validar();
-	});
-	function validar(){
-		var longitud = tarea.value.length;
-		var texto = tarea.value;
+	function validar() {
+		var longitud = tarea.value.length,
+			texto = tarea.value;
 
-		if(longitud == 0 || texto === "" || texto.trim().length === 0){
-			btnNuevaTarea.disabled=true;
+		if (longitud === 0 || texto === "" || texto.trim().length === 0) {
+			btnNuevaTarea.disabled = true;
 			return false;
-		}
-		else{
-			btnNuevaTarea.disabled=false;
+		} else {
+			btnNuevaTarea.disabled = false;
 		}
 	}
+	tarea.addEventListener("keydown", function () {
+		validar();
+	});
 
-function agregarTarea() {
-		var tareas = tarea.value;
-		var contenedor = document.getElementById("contenedor");
-		var nuevaTarea= document.createElement("div");
+	function agregarTarea() {
+		var tareas = tarea.value,
+			contenedor = document.getElementById("contenedor"),
+			nuevaTarea = document.createElement("div"),
+			parrafo = document.createElement("p"),
+			checkbox = document.createElement("input"),
+			texto = document.createTextNode(tareas),
+			icono = document.createElement("span");
+
 		nuevaTarea.classList.add("lista");
 		nuevaTarea.classList.add("checkbox");
 		contenedor.appendChild(nuevaTarea);
-		var parrafo = document.createElement("p");
-		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
 		parrafo.appendChild(checkbox);
 		nuevaTarea.appendChild(parrafo);
-		var texto = document.createTextNode(tareas);
 		parrafo.appendChild(texto);
 		parrafo.classList.add("contenedorLista");
-
-		var icono = document.createElement("span");
-		icono.classList.add("fa","fa-trash-o","icono");
+		icono.classList.add("fa", "fa-trash-o", "icono");
 		nuevaTarea.appendChild(icono);
 
-		checkbox.addEventListener("click", function() {
-			linea();
-		});
-		function linea(){
+		function linea() {
 			parrafo.classList.toggle("linea");
 		}
-		icono.addEventListener("click", function() {
-			borrarTarea();
+
+		checkbox.addEventListener("click", function () {
+			linea();
 		});
-		function borrarTarea(){
+
+		function borrarTarea() {
 			contenedor.removeChild(nuevaTarea);
 		}
+		icono.addEventListener("click", function () {
+			borrarTarea();
+		});
 	}
 
 	/* Eventos */
-	btnNuevaTarea.addEventListener("click", function() {
+	btnNuevaTarea.addEventListener("click", function () {
 		agregarTarea();
-		btnNuevaTarea.disabled=true;
-		tarea.value="";
+		btnNuevaTarea.disabled = true;
+		tarea.value = "";
 	});
-
-
-
 };
